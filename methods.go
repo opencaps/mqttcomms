34c4c41c8da3ec98624b37d2caf646d1554b750f
Bryan De Faria <bryan.defaria@ubiant.com>
2024-10-14 11:40:29 +0200

    Add methods

--- methods.go
+++ methods.go
@@ -2,9 +2,10 @@ package mqttcomms
 
 const (
 	// Device methods
-	DEV_ADD_NEW = 0x00
-	DEV_ADD     = 0x01
-	DEV_REMOVE  = 0x02
+	DEV_ADD_NEW     = 0x00
+	DEV_ADD         = 0x01
+	DEV_REMOVE      = 0x02
+	DEV_UPDATE_ZONE = 0x03
 
 	// Admin methods
 	AD_INIT_UPDATE     = 0x00
@@ -21,4 +22,13 @@ const (
 	// Time methods
 	TM_GET_TIME = 0x00
 	TM_PUBLISH  = 0x01
+
+	// Calendar methods
+	CL_SET_ZONE_PLANNING  = 0x00
+	CL_SET_ZONE_EXCEPTION = 0x01
+	CL_GET_ZONE_PLANNING  = 0x02
+	CL_GET_ZONE_EXCEPTION = 0x03
+
+	// Telemetry methods
+	TELEMETRY = 0x00
 )
